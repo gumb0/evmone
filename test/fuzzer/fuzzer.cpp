6500c07f02278371eb21760ead0d26ce2a4e80cb
Paweł Bylica <chfast@gmail.com>
2019-05-28 09:29:58 +0200

    fuzzer: Check output

--- test/fuzzer/fuzzer.cpp
+++ test/fuzzer/fuzzer.cpp
@@ -2,12 +2,11 @@
 // Copyright 2019 Pawel Bylica.
 // Licensed under the Apache License, Version 2.0.
 
-#include <evmc/evmc.hpp>
 #include <evmone/evmone.h>
 
-#include <evmc/helpers.hpp>
 #include <evmc/instructions.h>
 #include <intx/intx.hpp>
+#include <test/utils/host_mock.hpp>
 #include <test/utils/utils.hpp>
 #include <algorithm>
 #include <iostream>
@@ -16,100 +15,6 @@
 
 extern "C" evmc_instance* evmc_create_interpreter() noexcept;
 
-class Host : public evmc_context
-{
-public:
-    evmc_address last_accessed_account = {};
-
-    std::unordered_map<evmc_bytes32, evmc_bytes32> storage;
-    bool storage_cold = true;
-
-    evmc_tx_context tx_context = {};
-
-    bytes log_data;
-    std::vector<evmc_bytes32> log_topics;
-
-    evmc_address selfdestruct_beneficiary = {};
-
-    evmc_bytes32 blockhash = {};
-
-    bool exists = false;
-    intx::uint256 balance = {};
-    bytes extcode = {};
-
-    evmc_message call_msg = {};  ///< Recorded call message.
-    evmc_result call_result = {};
-
-    static evmc_host_interface interface;
-
-    Host() noexcept : evmc_context{&interface} {}
-};
-
-evmc_host_interface Host::interface = {
-    [](evmc_context* ctx, const evmc_address* addr) {
-        auto& e = *static_cast<Host*>(ctx);
-        e.last_accessed_account = *addr;
-        return e.exists;
-    },
-    [](evmc_context* ctx, const evmc_address*, const evmc_bytes32* key) {
-        return static_cast<Host*>(ctx)->storage[*key];
-    },
-    [](evmc_context* ctx, const evmc_address*, const evmc_bytes32* key, const evmc_bytes32* value) {
-        auto& old = static_cast<Host*>(ctx)->storage[*key];
-
-        evmc_storage_status status;
-        if (old == *value)
-            status = EVMC_STORAGE_UNCHANGED;
-        else if (is_zero(old))
-            status = EVMC_STORAGE_ADDED;
-        else if (is_zero(*value))
-            status = EVMC_STORAGE_DELETED;
-        else if (static_cast<Host*>(ctx)->storage_cold)
-            status = EVMC_STORAGE_MODIFIED;
-        else
-            status = EVMC_STORAGE_MODIFIED_AGAIN;
-
-        old = *value;
-        return status;
-    },
-    [](evmc_context* ctx, const evmc_address* addr) {
-        auto& e = *static_cast<Host*>(ctx);
-        e.last_accessed_account = *addr;
-        evmc_uint256be b = {};
-        intx::be::store(b.bytes, e.balance);
-        return b;
-    },
-    [](evmc_context* ctx, const evmc_address* addr) {
-        auto& e = *static_cast<Host*>(ctx);
-        e.last_accessed_account = *addr;
-        return e.extcode.size();
-    },
-    [](evmc_context* ctx, const evmc_address* addr) {
-        auto& e = *static_cast<Host*>(ctx);
-        e.last_accessed_account = *addr;
-        auto hash = evmc_bytes32{};
-        std::fill(std::begin(hash.bytes), std::end(hash.bytes), uint8_t{0xee});
-        return hash;
-    },
-    [](evmc_context*, const evmc_address*, size_t, uint8_t*, size_t) { return size_t{0}; },
-    [](evmc_context* ctx, const evmc_address*, const evmc_address* beneficiary) {
-        static_cast<Host*>(ctx)->selfdestruct_beneficiary = *beneficiary;
-    },
-    [](evmc_context* ctx, const evmc_message* m) {
-        auto& e = *static_cast<Host*>(ctx);
-        e.call_msg = *m;
-        return e.call_result;
-    },
-    [](evmc_context* ctx) { return static_cast<Host*>(ctx)->tx_context; },
-    [](evmc_context* ctx, int64_t) { return static_cast<Host*>(ctx)->blockhash; },
-    [](evmc_context* ctx, const evmc_address*, const uint8_t* data, size_t data_size,
-        const evmc_bytes32 topics[], size_t topics_count) {
-        auto& e = *static_cast<Host*>(ctx);
-        e.log_data.assign(data, data_size);
-        e.log_topics.reserve(topics_count);
-        std::copy_n(topics, topics_count, std::back_inserter(e.log_topics));
-    },
-};
 
 static auto evmone = evmc::vm{evmc_create_evmone()};
 static auto aleth = evmc::vm{evmc_create_interpreter()};
@@ -127,8 +32,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t data_size) noe
     auto code = &data[2];
     auto code_size = data_size - 2;
 
-    auto ctx1 = Host{};
-    auto ctx2 = Host{};
+    auto ctx1 = MockedHost{};
+    auto ctx2 = MockedHost{};
 
     auto r1 = evmone.execute(ctx1, EVMC_PETERSBURG, msg, code, code_size);
     auto r2 = aleth.execute(ctx2, EVMC_PETERSBURG, msg, code, code_size);
@@ -147,7 +52,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t data_size) noe
 
     if (sc1 != sc2)
     {
-        std::cerr << "status code: " << r1.status_code << " vs " << r2.status_code << "\n";
+        std::cerr << "status code: evmone:" << r1.status_code << " vs aleth:" << r2.status_code
+                  << "\n";
         __builtin_trap();
     }
 
@@ -158,7 +64,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t data_size) noe
         __builtin_trap();
     }
 
-    if (r1.output_size != r2.output_size)
+    if (bytes_view{r1.output_data, r1.output_size} != bytes_view{r2.output_data, r2.output_size})
         __builtin_trap();
 
     return 0;
