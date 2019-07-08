ad87a8c58fe4f62059172a11a51ee9cf935d24a9
Paweł Bylica <chfast@gmail.com>
2019-07-08 14:28:39 +0200

    cmake: Use latest intx

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,5 @@
+# evmone: Fast Ethereum Virtual Machine implementation
+# Copyright 2019 The evmone Authors.
+# Licensed under the Apache License, Version 2.0.
+
+hunter_config(intx URL https://github.com/chfast/intx/archive/be_api.tar.gz SHA1 63c1a61836be1a69f8407e5c321f1d1f477210fc)
